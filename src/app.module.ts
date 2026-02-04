cb9ab0d9ce8d3b50eb95d491c13d820745a4b7f0
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2026-02-04 13:43:30 -0300

    Ambiente de dev e prod e deploy do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,40 +2,30 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteModule } from './clientes/clientes.module';
 import { TelefoneModule } from './telefones/telefones.module';
-import { Telefones } from './telefones/entities/telefones.entity';
-import { Clientes } from './clientes/entities/clientes.entity';
 import { EnderecoModule } from './enderecos/enderecos.module';
-import { Enderecos } from './enderecos/enderecos.module';
-import { Equipamentos } from './equipamentos/entities/equipamentos.entity';
 import { EquipamentoModule } from './equipamentos/equipamentos.module';
 import { CategoriaModule } from './categorias/categorias.module';
-import { Categorias } from './categorias/entities/categorias.entity';
 import { AluguelItemModule } from './aluguel_item/alguel_item.module';
-import { AluguelItem } from './aluguel_item/entities/alguel_item.entity';
-import { Aluguel } from './alugueis/entities/aluguel.entity';
 import { AluguelModule } from './alugueis/aluguel.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.services';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_rentacycle',
-      entities: [Clientes, Telefones, Enderecos, Equipamentos, Categorias, AluguelItem, Aluguel],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
+
     ClienteModule,
     TelefoneModule,
     EnderecoModule,
-    EquipamentoModule, 
+    EquipamentoModule,
     CategoriaModule,
     AluguelItemModule,
-    AluguelModule
+    AluguelModule,
   ],
 
   controllers: [AppController],
